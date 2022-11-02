c8e4141d9688a702538acad6859531e50304dbd1
H0lgi <103411678+H0lgi@users.noreply.github.com>
2022-11-02 11:09:09 +0100

    added simple addition

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -l
 
-echo "Hello $1"
-time=$(date)
-echo "time=$time" >> $GITHUB_OUTPUT
+file = /data/test/input.txt
+awk '{ sum += $file } END { print sum }' file >> sum
+echo "summe=$sum" >> $GITHUB_OUTPUT
